e50f7ae8773e793f79e6a086753a9e54e0406d40
saducandy <adoniasandarge@gmail.com>
2023-05-20 09:37:27 +0100

    Creating the project

---
+++ src/main/java/com/example/demo/Project_IFRS16_Lease.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project_IFRS16_Lease {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project_IFRS16_Lease.class, args);
+    }
+
+}
